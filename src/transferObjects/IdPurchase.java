c53d583598382bc4e1fa0480f0739deb3bb84f82
Alex Hurtado <lexurt@gmail.com>
2022-01-30 23:48:51 +0100

    Versión inicial Purchase en el cliente

---
+++ src/transferObjects/IdPurchase.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transferObjects;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+/**
+ *
+ * @author ibai Arriola
+ */
+
+@XmlRootElement
+public class IdPurchase implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // la id de de un juego
+    private Integer idGame;
+    // la id de de un  cliente
+    private Integer idClient;
+    //getter y setter
+    public Integer getIdGame() {
+        return idGame;
+    }
+
+    public void setIdGame(Integer idGame) {
+        this.idGame = idGame;
+    }
+
+    public Integer getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(Integer idClient) {
+        this.idClient = idClient;
+    }
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idGame != null ? idGame.hashCode() : 0);
+        hash += (idClient != null ? idClient.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof IdPurchase)) {
+            return false;
+        }
+        IdPurchase other = (IdPurchase) object;
+        if ((this.idGame == null && other.idGame != null) || (this.idGame != null && !this.idGame.equals(other.idGame))) {
+            return false;
+        }
+        if ((this.idClient == null && other.idClient != null) || (this.idClient != null && !this.idClient.equals(other.idClient))) {
+            return false;
+        }
+        return true;
+    }
+// muestra la salida  de  idpurchase
+    @Override
+    public String toString() {
+        return "IdPurchase{" + "idGame=" + idGame + ", idClient=" + idClient + '}';
+    }
+}
--- src/transferObjects/Purchase.java
+++ src/transferObjects/Purchase.java
@@ -5,10 +5,97 @@
  */
 package transferObjects;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
 /**
  *
- * @author ibai Arriola
+ * @author Alex Hurtado
  */
-public class Purchase {
+@XmlRootElement
+public class Purchase implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    private IdPurchase idPurchase;
+/**
+ * Fecha en la que se produjo la compra
+ */
+
+    private Date purchaseDate;
+/**
+ * id del cliente que hizo la compra
+ */
+    private Client client;
+/**
+ * id del juego que se compro
+ */
     
+    private Game game;
+   
+   //Metodos getters y setters 
+    public IdPurchase getIdPurchase() {
+        return idPurchase;
+    }
+
+    public void setIdPurchase(IdPurchase idPurchase) {
+        this.idPurchase = idPurchase;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    @XmlTransient
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idPurchase != null ? idPurchase.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Purchase)) {
+            return false;
+        }
+        Purchase other = (Purchase) object;
+        if ((this.idPurchase == null && other.idPurchase != null) || (this.idPurchase != null && !this.idPurchase.equals(other.idPurchase))) {
+            return false;
+        }
+        return true;
+    }
+/**
+ * 
+ * @return devuelve los datos 
+ */
+    @Override
+    public String toString() {
+        return "Purchase{" + "idPurchase=" + idPurchase + ", purchaseDate=" + purchaseDate + ", client=" + client + ", game=" + game + '}';
+    }
+
 }
